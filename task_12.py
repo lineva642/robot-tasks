451e605234a8716d3383428c56adb0db05f33822
Vaace <pipyaka3@gmail.com>
2016-09-27 12:00:28 +0300

    1-12

--- task_12.py
+++ task_12.py
@@ -5,7 +5,14 @@ from pyrob.api import *
 
 @task
 def task_8_6():
-    pass
+   i=1
+   while i!=0:
+       if wall_is_beneath()==True and wall_is_above()==False:
+           fill_cell()
+       if wall_is_on_the_right()==True:
+           i=0
+       else:
+           move_right()
 
 
 if __name__ == '__main__':
